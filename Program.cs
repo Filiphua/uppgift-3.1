c389070ff98f40575aa75f4b5986257a6c2c6c1f
Filip <fihu08001@utb.vaxjo.se>
2025-11-28 08:21:15 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace uppgift_3_1
+{
+    class program
+    { 
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hur gammal är du");
+            int age = int.Parse(Console.ReadLine());
+            if (age > 19)
+            {
+                Console.WriteLine("Du är för gammal för att delta i tävlingen");
+            }
+            else if (age < 16)
+            {
+                Console.WriteLine("Du är för ung för att delta i tävlingen");
+            }
+            else
+            {
+                Console.WriteLine("Du är gammal nog att delta i tävlingen");
+            }
+        }
+    }
+}
